9aa03e4bee8fed4f2e3cfc7a8f4da6602b07a6e5
hyxw <2376539013@qq.com>
2019-12-08 20:53:11 +0800

    进行redis的缓存时间配置

--- redis/src/main/java/com/b505/redis/redis/RedisApplication.java
+++ redis/src/main/java/com/b505/redis/redis/RedisApplication.java
@@ -2,8 +2,15 @@ package com.b505.redis.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+/**
+ * 描述：redis学习项目启动类
+ * author:yulin
+ * Create date 2019-12-8 19:50
+ */
 @SpringBootApplication
+@EnableRedisRepositories
 public class RedisApplication {
 
     public static void main(String[] args) {
